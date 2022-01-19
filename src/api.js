55abba69e743531ee118261bd7af6bf7d9108dba
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-01-19 10:39:00 -0500

    get detail for each game

--- src/api.js
+++ src/api.js
@@ -43,3 +43,5 @@ ordering=-released&page_size=10`;
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}?key=${api_key}`
+export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots?key=${api_key}`
